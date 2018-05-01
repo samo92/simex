3a6597fb17a770d2763d2a9768213be094408753
Samo92 <damianmaciel92@gmail.com>
2018-05-01 17:04:37 -0500

    custombar added

--- app/src/main/java/ib/facmed/unam/mx/simexfacmed/MainActivity.java
+++ app/src/main/java/ib/facmed/unam/mx/simexfacmed/MainActivity.java
@@ -3,6 +3,7 @@ package ib.facmed.unam.mx.simexfacmed;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
@@ -27,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.appbar_main);
+        setSupportActionBar(toolbar);
     }
 
     public void goMaps(View view) {

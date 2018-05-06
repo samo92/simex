ddaa38ff7c346c5103dad40d3440844058b6194c
Samo92 <damianmaciel92@gmail.com>
2018-05-05 22:06:54 -0500

    added view detalle Eventos

---
+++ app/src/main/java/ib/facmed/unam/mx/simexfacmed/DetalleEventosActivity.java
@@ -0,0 +1,28 @@
+package ib.facmed.unam.mx.simexfacmed;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class DetalleEventosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_eventos);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}

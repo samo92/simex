5fa6171de816853308fe12413e4106c7e96e91bc
Samo92 <damianmaciel92@gmail.com>
2018-05-23 13:23:33 -0500

    seleccionarMapas and activity_programas new view style

--- app/src/main/java/ib/facmed/unam/mx/simexfacmed/SeleccionaMapasActivity.java
+++ app/src/main/java/ib/facmed/unam/mx/simexfacmed/SeleccionaMapasActivity.java
@@ -6,26 +6,31 @@ import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SeleccionaMapasActivity extends AppCompatActivity {
 
-    Button btn_palacio;
-    Button btn_posgrado;
-    Button btn_medicina;
+    TextView btn_palacio;
+    TextView btn_posgrado;
+    TextView btn_medicina;
+    TextView btn_inr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selecciona_mapas);
 
-        btn_palacio = (Button) findViewById(R.id.btn_mapa_palacio);
+        btn_palacio = (TextView) findViewById(R.id.btn_mapa_palacio);
         btn_palacio.setText(R.string.button_palacio_sede);
 
-        btn_posgrado = (Button) findViewById(R.id.btn_mapa_posgrado);
+        btn_posgrado = (TextView) findViewById(R.id.btn_mapa_posgrado);
         btn_posgrado.setText(getText(R.string.button_palacio_posgrado));
 
-        btn_medicina = (Button) findViewById(R.id.btn_mapa_facmed);
+        btn_medicina = (TextView) findViewById(R.id.btn_mapa_facmed);
         btn_medicina.setText(getText(R.string.button_palacio_medicina));
+
+        btn_inr = (TextView) findViewById(R.id.btn_mapa_inr);
+        btn_inr.setText(getText(R.string.button_palacio_inr));
     }
 
     public void goShowMaps(View view) {
